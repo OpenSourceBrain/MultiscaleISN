52a17056960e64cbea476935ebd7f4035c3cee86
Padraig Gleeson <p.gleeson@gmail.com>
2018-10-10 16:53:41 +0100

    Script for running multiple simulations

---
+++ runall.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+# This script requires the repo https://github.com/OpenSourceBrain/SadehEtAl2017-InhibitionStabilizedNetworks 
+# to be present in the directory ../SadehEtAl2017-InhibitionStabilizedNetworks
+
+# This path is required for https://github.com/OpenSourceBrain/SadehEtAl2017-InhibitionStabilizedNetworks/blob/master/SpikingSimulationModels/defaultParams.py
+export PYTHONPATH=PYTHONPATH:../SadehEtAl2017-InhibitionStabilizedNetworks/SpikingSimulationModels
+
+for i in `seq 100 100 4000`;
+do
+    echo "==============================="
+    echo "Running with seed: "$i
+    python ISN.py -AllenCells -perturbation -netpyne -seed:$i
+    python to_gdf.py
+    python3 ../SadehEtAl2017-InhibitionStabilizedNetworks/PyNN/analysis_perturbation_pynn.py .9 1000 -nogui -average -legend
+done 
---
+++ to_gdf.py
@@ -0,0 +1,42 @@
+import os, sys
+
+def convert(base_dir='./temp/', detailed_also = False):
+    
+    files = ['%sSim_ISN_net.popExc.spikes'%base_dir,'%sSim_ISN_net.popInh.spikes'%base_dir]
+    
+    if detailed_also:
+        files = ['%sSim_ISN_net.popExc.spikes'%base_dir,'%sSim_ISN_net.popInh.spikes'%base_dir, '%sSim_ISN_net.popExc2.spikes'%base_dir]
+        
+
+    out_file = 'ISN-nest-EI-0.gdf'
+    out = open(out_file,'w')
+
+    for fn in files:
+        f = open(fn)
+
+        for l in f.readlines():
+            w  = l.split('\t')
+            i = int(w[0])
+            t = float(w[1])*1000
+            if 'Inh' in fn:
+                i+=800
+            if 'Exc2' in fn:
+                i+=790
+
+            out.write('%s   %s\n'%(t, i))
+            
+        print("> Converted: %s"%os.path.abspath(fn))
+        
+    print("> Saved:     %s"%os.path.abspath(out_file))
+        
+    out.close()
+
+
+if __name__ == '__main__':
+    
+    
+    detailed_also = '-detailed' in sys.argv
+        
+    convert(detailed_also=detailed_also)
+        
+        
